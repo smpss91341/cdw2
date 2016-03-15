6ebf1e29c7379eb8c86eeae61ffea9fe1d79b098
coursemdetw <course@mde.tw>
2016-03-15 16:33:22 +0800

    加入網際協同 2D 繪圖架構, 並清理資料

--- users/s2b/g99/task0.py
+++ users/s2b/g99/task0.py
@@ -13,4 +13,67 @@ def task1():
     #return "bg99 task1"
     return render_template('task1.html', var1="來自 bg99 的 task1 變數")
     
+# 展示傳回 Brython 程式
+@bg99.route('/task3')
+def task3():
+    outstring = '''
+from javascript import JSConstructor
+from browser import window
+import math
+
+cango = JSConstructor(window.Cango)
+cobj = JSConstructor(window.Cobj)
+shapedefs = window.shapeDefs
+obj2d = JSConstructor(window.Obj2D)
+cgo = cango("plotarea")
+
+cgo.setWorldCoords(-250, -250, 500, 500) 
+
+# 決定要不要畫座標軸線
+cgo.drawAxes(0, 240, 0, 240, {
+    "strokeColor":"#aaaaaa",
+    "fillColor": "#aaaaaa",
+    "xTickInterval": 20,
+    "xLabelInterval": 20,
+    "yTickInterval": 20,
+    "yLabelInterval": 20})
+        
+#cgo.drawText("使用 Cango 繪圖程式庫!", 0, 0, {"fontSize":60, "fontWeight": 1200, "lorg":5 })
+
+deg = math.pi/180  
+
+def task3(x, y, rx, ry, rot, color, border, linewidth):
+    # 旋轉必須要針對相對中心 rot not working yet
+    chamber = "M -6.8397, -1.4894 \
+                     A 7, 7, 0, 1, 0, 6.8397, -1.4894 \
+                     A 40, 40, 0, 0, 1, 6.8397, -18.511 \
+                     A 7, 7, 0, 1, 0, -6.8397, -18.511 \
+                     A 40, 40, 0, 0, 1, -6.8397, -1.4894 z"
+    cgoChamber = window.svgToCgoSVG(chamber)
+    cmbr = cobj(cgoChamber, "SHAPE", {
+            "fillColor": color,
+            "border": border,
+            "strokeColor": "tan",
+            "lineWidth": linewidth })
+
+    # 複製 cmbr, 然後命名為 basic1
+    basic1 = cmbr.dup()
+    # basic1 轉 -120 度
+    basic1.rotate(-120)
+
+    cmbr.appendPath(basic1)
+    
+    # hole 為原點位置
+    hole = cobj(shapedefs.circle(4), "PATH")
+    cmbr.appendPath(hole)
+
+    # 表示放大 3 倍
+    #cgo.render(cmbr, x, y, 3, rot)
+    # 放大 3 倍
+    cgo.render(cmbr, x, y, 3, rot)
+
+task3(-100, -130, 0, 0, 0, "blue", True, 4)
+'''
+    return outstring
+    
     
