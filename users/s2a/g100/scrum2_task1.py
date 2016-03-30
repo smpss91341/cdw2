998aeefa521123cc713ea4f2dd54558467bb7fd8
coursemdetw <course@mde.tw>
2016-03-30 19:47:31 +0800

    送回 ag100 scrum-2 組員的繪圖, 並且與 scrum-1 整合

--- users/s2a/g100/scrum2_task1.py
+++ users/s2a/g100/scrum2_task1.py
@@ -2,36 +2,12 @@
 from flask import Blueprint, render_template
 
 # 利用 Blueprint建立 ag1, 並且 url 前綴為 /ag1, 並設定 template 存放目錄
-scrum1_task1 = Blueprint('scrum1_task1', __name__, url_prefix='/ag100', template_folder='templates')
+scrum2_task1 = Blueprint('scrum2_task1', __name__, url_prefix='/ag100', template_folder='templates')
 
 # scrum1_task1 為完整可以單獨執行的繪圖程式
-@scrum1_task1.route('/scrum1_task1')
+@scrum2_task1.route('/scrum2_task1')
 def task1():
     outstring = '''
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>網際 2D 繪圖</title>
-    <!-- IE 9: display inline SVG -->
-    <meta http-equiv="X-UA-Compatible" content="IE=9">
-<script type="text/javascript" src="http://brython.info/src/brython_dist.js"></script>
-<script type="text/javascript" src="http://cptocadp-2015fallhw.rhcloud.com/static/Cango-8v03.js"></script>
-<script type="text/javascript" src="http://cptocadp-2015fallhw.rhcloud.com/static/Cango2D-6v13.js"></script>
-<script type="text/javascript" src="http://cptocadp-2015fallhw.rhcloud.com/static/CangoAxes-1v33.js"></script>
-
-</head>
-<body>
-
-<script>
-window.onload=function(){
-brython(1);
-}
-</script>
-
-<canvas id="plotarea" width="800" height="800"></canvas>
-
-<script type="text/python">
 from javascript import JSConstructor
 from browser import window
 import math
@@ -104,14 +80,7 @@ def O(x, y, rx, ry, rot, color, border, linewidth):
     # 放大 5 倍
     cgo.render(cmbr, x, y, 5, rot)
 
-O(0, 0, 0, 0, 0, "lightyellow", True, 4)
-</script>
-<!-- 以協同方式加上 ag100 的 scrum-2 組員所寫的 task1 程式碼 -->
-<script type="text/python" src="/ag100/scrum2_task1"></script>
-<!-- 以協同方式加上 ag100 的  scrum-3 組員所寫的 task1 程式碼 -->
-<!-- <script type="text/python" src="/ag100/scrum3_task1"></script>-->
-</body>
-</html>
+O(-173, 0, 0, 0, 0, "lightgreen", True, 4)
 '''
     return outstring
     
