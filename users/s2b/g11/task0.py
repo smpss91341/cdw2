74d67b0629b0b0d323275a041476186dde58e2a2
40323250 <40323250@gm.nfu.edu.tw>
2016-04-28 13:45:00 +0800

    recover by  0d5d6879a288de60af27050bf526cabc2f9d6cab version

---
+++ users/s2b/g11/task0.py
@@ -0,0 +1,112 @@
+# 各組分別在各自的 .py 程式中建立應用程式 (第1步/總共3步)
+from flask import Blueprint, render_template
+
+# 利用 Blueprint建立 ag1, 並且 url 前綴為 /ag1, 並設定 template 存放目錄
+bg11 = Blueprint('bg11', __name__, url_prefix='/bg11', template_folder='templates')
+
+@bg11.route('/task0')
+def task0():
+    return "bg11 task0"
+    
+@bg11.route('/task1')
+def task1():
+    #return "bg11 task1"
+    return render_template('task1.html', var1="來自 bg11 的 task1 變數")
+
+# 展示傳回 Brython 程式
+@bg11.route('/task2')
+def task2():
+    outstring = '''
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>網際 2D 繪圖</title>
+    <!-- IE 9: display inline SVG -->
+    <meta http-equiv="X-UA-Compatible" content="IE=9">
+<script type="text/javascript" src="http://brython.info/src/brython_dist.js"></script>
+<script type="text/javascript" src="http://cptocadp-2015fallhw.rhcloud.com/static/Cango-8v03.js"></script>
+<script type="text/javascript" src="http://cptocadp-2015fallhw.rhcloud.com/static/Cango2D-6v13.js"></script>
+<script type="text/javascript" src="http://cptocadp-2015fallhw.rhcloud.com/static/CangoAxes-1v33.js"></script>
+
+</head>
+<body>
+
+<script>
+window.onload=function(){
+brython(1);
+}
+</script>
+
+<canvas id="plotarea" width="800" height="800"></canvas>
+
+<script type="text/python">
+from javascript import JSConstructor
+from browser import window
+import math
+
+cango = JSConstructor(window.Cango)
+cobj = JSConstructor(window.Cobj)
+shapedefs = window.shapeDefs
+obj2d = JSConstructor(window.Obj2D)
+cgo = cango("plotarea")
+
+cgo.setWorldCoords(-250, -250, 500, 500) 
+
+# 決定要不要畫座標軸線
+cgo.drawAxes(0, 240, 0, 240, {
+    "strokeColor":"#aaaaaa",
+    "fillColor": "#aaaaaa",
+    "xTickInterval": 20,
+    "xLabelInterval": 20,
+    "yTickInterval": 20,
+    "yLabelInterval": 20})
+        
+#cgo.drawText("使用 Cango 繪圖程式庫!", 0, 0, {"fontSize":60, "fontWeight": 1200, "lorg":5 })
+deg = math.pi/180  
+def O(x, y, rx, ry, rot, color, border, linewidth):
+    # 旋轉必須要針對相對中心 rot not working yet
+    chamber = "M -6.8397, -1.4894 \
+                     A 7, 7, 0, 1, 0, 6.8397, -1.4894 \
+                     A 40, 40, 0, 0, 1, 6.8397, -18.511 \
+                     A 7, 7, 0, 1, 0, -6.8397, -18.511 \
+                     A 40, 40, 0, 0, 1, -6.8397, -1.4894 z"
+    cgoChamber = window.svgToCgoSVG(chamber)
+    cmbr = cobj(cgoChamber, "SHAPE", {
+            "fillColor": color,
+            "border": border,
+            "strokeColor": "tan",
+            "lineWidth": linewidth })
+  
+
+    cmbr.translate(0, 20)
+   
+    
+    
+    # hole 為原點位置
+    #hole = cobj(shapedefs.circle(4), "PATH") 
+    #cmbr.appendPath(hole) 
+
+    # 放大 1 倍
+    cgo.render(cmbr, x, y, 1, rot)
+    
+O(0, 0, 0, 0, 0, "lightyellow", True, 4)
+</script>
+
+<script type="text/python" src="http://2016spring-40323250.rhcloud.com/bg9/scrum1_50"></script>
+<script type="text/python" src="http://2016spring-40323250.rhcloud.com/bg9/scrum2_18"></script>
+<script type="text/python" src="http://2016spring-40323250.rhcloud.com/bg9/scrum3_14"></script>
+<script type="text/python" src="http://2016spring-40323250.rhcloud.com/bg9/scrum4_31"></script>
+<script type="text/python" src="http://2016spring-40323250.rhcloud.com/bg9/scrum5_30"></script>
+<script type="text/python" src="http://2016spring-40323250.rhcloud.com/bg9/scrum6_33"></script>
+
+
+
+
+
+</body>
+</html>
+'''
+    return outstring
+    
+
